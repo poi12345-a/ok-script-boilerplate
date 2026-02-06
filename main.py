014cc15419a29234c82df67218682a068326760a
poi12345-a <aisyeyu@gmail.com>
2026-02-06 17:01:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import ctypes
 import ok
 from src.config import config
 
 if __name__ == '__main__':
+    ctypes.windll.user32.SetProcessDPIAware()
     config = config
     ok = ok.OK(config)
     ok.start()
